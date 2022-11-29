9833fc0ef098455dc4920d4d26731f5545d5a81b
thunderpirate <78709989+thunderpirate@users.noreply.github.com>
2022-11-28 22:57:39 -0400

    Update script.py

--- script.py
+++ script.py
@@ -85,7 +85,7 @@ def get_success_points(my_list):
 
 to_publish['group success points']=to_publish[['Team 1','Team 2','Team 3','Team 4']].apply(get_success_points, axis=1)
 to_publish['total tournament points']=to_publish['group success points']+to_publish['group game points']
-to_publish.rename(columns = {'total tournament points':'Total points','group games played'}, inplace = True)
+to_publish.rename(columns = {'total tournament points':'Total points'}, inplace = True)
 to_publish=to_publish[['Total points','group success points','group game points','played','ppg','Team 1','Team 2','Team 3','Team 4']]
 
 st.dataframe(data=to_publish, height=my_df.shape[0]*50, use_container_width=True)
