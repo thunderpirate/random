802e160d2d4b5815e05bd37b3a39c27a938af3db
thunderpirate <78709989+thunderpirate@users.noreply.github.com>
2022-12-07 22:41:23 -0400

    Update script.py

--- script.py
+++ script.py
@@ -166,11 +166,13 @@ to_publish=to_publish.sort_values(by=['Total Points'],ascending=False)
 st.set_page_config(layout="wide")
 st.dataframe(data=to_publish, use_container_width=True)
 
+st.write('Upcoming fixtures')
 trimmed_fixtures = fixtures_df[fixtures_df['winner'].isnull()]
 trimmed_fixtures = trimmed_fixtures[['stage_name','home_name','away_name','datetime']]
 
 trimmed_fixtures['date']=pd.to_datetime(trimmed_fixtures['datetime']).dt.date
 trimmed_fixtures['time']=pd.to_datetime(trimmed_fixtures['datetime']).dt.tz_convert('Canada/Atlantic').dt.time
 trimmed_fixtures = trimmed_fixtures[['stage_name','home_name','away_name','date','time']]
-trimmed_fixtures.rename(columns = {'stage_name':'Stage','home_name':'Team 1','away_name':'Team 2'}, inplace = True).reset_index(drop=True)
+trimmed_fixtures.rename(columns = {'stage_name':'Stage','home_name':'Team 1','away_name':'Team 2'}, inplace = True)
+trimmed_fixtures.reset_index(drop=True)
 st.dataframe(data=trimmed_fixtures)
